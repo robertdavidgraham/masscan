7bef9994a09260e179f975dc452fd1c0601f16ab
robertdavidgraham <robert_david_graham@yahoo.com>
2014-01-11 12:22:51 -0500

    --hello-string

--- src/crypto-base64.h
+++ src/crypto-base64.h
@@ -1,5 +1,6 @@
 #ifndef CRYPTO_BASE64_H
 #define CRYPTO_BASE64_H
+#include <stdio.h>
 
 size_t base64_decode(void *dst, size_t sizeof_dst, const void *src, size_t sizeof_src);
 size_t base64_encode(void *dst, size_t sizeof_dst, const void *src, size_t sizeof_src);
