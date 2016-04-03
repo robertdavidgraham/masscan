08642dc8e4c18169d5e9669bc09cf21c618fdc34
Adam Greene <agreene5@bloomberg.net>
2016-04-03 18:01:22 -0400

    Fixed a bug where usage of -iL/--include/include = would error out to masscan_usage() if you were doing an ICMP only scan. This could arguably be fixed elsewhere, like in the --ping handling, but I followed what seemed to be the convention of the code.
    To reproduce, before this patch:
    
    cat > range.txt << 'EOF'
    1.2.3.4
    3.4.5.6
    EOF
    masscan --ping -iL range.txt
    
    After the patch, the ping scan runs as expected

--- src/main-conf.c
+++ src/main-conf.c
@@ -1305,6 +1305,8 @@ masscan_set_parameter(struct Masscan *masscan,
         masscan->op = Operation_List_Adapters;
     } else if (EQUALS("includefile", name)) {
         ranges_from_file(&masscan->targets, value);
+        if (masscan->op == 0)
+            masscan->op = Operation_Scan;
     } else if (EQUALS("infinite", name)) {
         masscan->is_infinite = 1;
     } else if (EQUALS("interactive", name)) {
