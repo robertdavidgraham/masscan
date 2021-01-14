28d2331e0e24e83b567708de02a19c8ead2f3de1
Robert Graham <robert_david_graham@yahoo.com>
2021-01-14 00:08:49 -0500

    host only output

---
+++ src/out-hostonly.c
@@ -0,0 +1,74 @@
+#include "output.h"
+#include "masscan.h"
+#include "masscan-app.h"
+#include "masscan-status.h"
+#include "unusedparm.h"
+#include "out-tcp-services.h"
+
+
+
+
+
+static void
+hostonly_out_open(struct Output *out, FILE *fp)
+{
+    UNUSEDPARM(out);
+}
+
+
+static void
+hostonly_out_close(struct Output *out, FILE *fp)
+{
+    UNUSEDPARM(out);
+}
+
+static void
+hostonly_out_status(struct Output *out, FILE *fp, time_t timestamp,
+    int status, ipaddress ip, unsigned ip_proto, unsigned port, unsigned reason, unsigned ttl)
+{
+    UNUSEDPARM(reason);
+    UNUSEDPARM(out);
+    UNUSEDPARM(timestamp);
+
+   fprintf(fp, "%s\n", ipaddress_fmt(ip).string);
+}
+
+
+/*************************************** *************************************
+ ****************************************************************************/
+static void
+hostonly_out_banner(struct Output *out, FILE *fp, time_t timestamp,
+        ipaddress ip, unsigned ip_proto, unsigned port,
+        enum ApplicationProtocol proto, unsigned ttl,
+        const unsigned char *px, unsigned length)
+{ /* SYN only - no banner */
+    UNUSEDPARM(out);
+    UNUSEDPARM(ttl);
+    UNUSEDPARM(port);
+    UNUSEDPARM(fp);
+    UNUSEDPARM(timestamp);
+    UNUSEDPARM(ip);
+    UNUSEDPARM(ip_proto);
+    UNUSEDPARM(proto);
+    UNUSEDPARM(px);
+    UNUSEDPARM(length);
+    fprintf(fp, "%s\n", ipaddress_fmt(ip).string);
+
+    return;
+} 
+ 
+
+
+/****************************************************************************
+ ****************************************************************************/
+const struct OutputType hostonly_output = {
+    "hostonly",
+    0,
+    hostonly_out_open,
+    hostonly_out_close,
+    hostonly_out_status,
+    hostonly_out_banner
+};
+
+
+
