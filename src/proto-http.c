9e302ce726eba66edba185c1ad1280fa5e5e72cf
Robert Graham <robert_david_graham@yahoo.com>
2021-01-31 03:59:28 -0500

    changed default User-Agent: to version 1.3

--- src/proto-http.c
+++ src/proto-http.c
@@ -120,7 +120,7 @@ http_change_field(unsigned char **inout_header, unsigned header_length,
  ***************************************************************************/
 static const char
 http_hello[] =      "GET / HTTP/1.0\r\n"
-                    "User-Agent: masscan/1.0 (https://github.com/robertdavidgraham/masscan)\r\n"
+                    "User-Agent: masscan/1.3 (https://github.com/robertdavidgraham/masscan)\r\n"
                     "Accept: */*\r\n"
                     //"Connection: Keep-Alive\r\n"
                     //"Content-Length: 0\r\n"
