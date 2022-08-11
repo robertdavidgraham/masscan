abd6d444ee5d1b285cbb0ffadb0b4405474708f6
taguchi-ch <taguchi.ch@gmail.com>
2022-08-11 17:56:58 +0900

    fix FreeBSD aarch64 build issue

--- src/smack1.c
+++ src/smack1.c
@@ -119,9 +119,8 @@
 #elif defined(__FreeBSD__)
 #include <sys/types.h>
 #include <machine/cpufunc.h>
-#define __rdtsc rdtsc
-#if (__ARM_ARCH >= 6)  // V6 is the earliest arch that has a standard cyclecount
-unsigned long long rdtsc(void)
+#if (__ARM_ARCH >= 6 && __ARM_ARCH <= 7)  // V6 is the earliest arch that has a standard cyclecount
+unsigned long long __rdtsc(void)
 {
   uint32_t pmccntr;
   uint32_t pmuseren;
@@ -138,6 +137,10 @@ unsigned long long rdtsc(void)
   }
   return 0;
 }
+#elif defined(__aarch64__)
+#define __rdtsc() 0
+#else
+#define __rdtsc rdtsc
 #endif
 #elif defined (__llvm__)
 #if defined(i386) || defined(__i386__)
